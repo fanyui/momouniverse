b91f1c7b3ef28380d2caa57b4582d0126873a8a6
Harisu Fanyui <fanyuiharisu@gmail.com>
2019-10-27 11:24:51 +0100

    Updates

---
+++ precache-manifest.5c0cb0db355c8da83be80a6dfa7869f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af9c52459b559e19438c0fdb263f5ff",
+    "url": "/momouniverse/index.html"
+  },
+  {
+    "revision": "be3e7c2429593904822e",
+    "url": "/momouniverse/static/css/main.e747113e.chunk.css"
+  },
+  {
+    "revision": "4ef99e05eef0c66a2fb1",
+    "url": "/momouniverse/static/js/2.9168c99c.chunk.js"
+  },
+  {
+    "revision": "be3e7c2429593904822e",
+    "url": "/momouniverse/static/js/main.3cf79bcd.chunk.js"
+  },
+  {
+    "revision": "f28f467853e4f1a131ac",
+    "url": "/momouniverse/static/js/runtime-main.3cb4d0da.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/momouniverse/static/media/logo.25bf045c.svg"
+  }
+]);
